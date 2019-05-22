dfac91053ca399abbe0912d36c5574def5a6d770
Alexandru Enache <alexandru.enache.10@gmail.com>
2019-05-23 00:08:02 +0300

    simple tests for various poco-s using [fromhybrid]

---
+++ FromHybridTests/Controllers/Commands.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FromHybridTests.Controllers
+{
+    public class Cmd1
+    {
+        public readonly int Id;
+
+        public Cmd1(int id)
+        {
+            Id = id;
+        }
+    }
+
+    public class Cmd2
+    {
+        public int Id { get; }
+
+        public Cmd2(int id)
+        {
+            Id = id;
+        }
+    }
+
+    public class Cmd3
+    {
+        public int Id { get; internal set; }
+
+        public Cmd3(int id)
+        {
+            Id = id;
+        }
+    }
+
+    public class Cmd4
+    {
+        public int Id { get; set; }
+    }
+
+    public class Cmd5
+    {
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+
+        public Cmd5(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+    public class Cmd6
+    {
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+    }
+}
---
+++ FromHybridTests/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using HybridModelBinding;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FromHybridTests.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("cmd1/{id}")]
+        public ActionResult<Cmd1> GetCmd1([FromHybrid] Cmd1 cmd1)
+        {
+            return cmd1;
+        }
+
+        [HttpGet("cmd2/{id}")]
+        public ActionResult<Cmd2> GetCmd2([FromHybrid] Cmd2 cmd2)
+        {
+            return cmd2;
+        }
+
+        [HttpGet("cmd3/{id}")]
+        public ActionResult<Cmd3> GetCmd3([FromHybrid] Cmd3 cmd3)
+        {
+            return cmd3;
+        }
+
+        [HttpGet("cmd4/{id}")]
+        public ActionResult<Cmd4> GetCmd4([FromHybrid] Cmd4 cmd4)
+        {
+            return cmd4;
+        }
+
+        [HttpPost("cmd5/{id}")]
+        public ActionResult<Cmd5> GetCmd5([FromHybrid] Cmd5 cmd5)
+        {
+            return cmd5;
+        }
+
+        [HttpPost("cmd6/{id}")]
+        public ActionResult<Cmd6> GetCmd6([FromHybrid] Cmd6 cmd6)
+        {
+            return cmd6;
+        }
+    }
+}
